ac0848be9344c49703e1d231e762d2b0157576b5
harikays <58000936+harikays@users.noreply.github.com>
2021-11-03 00:20:35 +0800

    final commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+
+import './assets/index.css';
+
+Vue.config.productionTip = true;
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    // { path: '/page', component: App },
+  ],
+});
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
